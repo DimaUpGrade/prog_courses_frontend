0b5a20c1aec98e440da20be913f251f7ef4073f3
Dmitry <dima2002bord@yandex.ru>
2024-02-08 07:33:15 +0300

    add bootstrap

---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <nav>
+            
+        </nav>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <NavBar></NavBar>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    NavBar
+  }
+}
+</script>
