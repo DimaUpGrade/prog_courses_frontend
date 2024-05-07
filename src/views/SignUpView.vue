b72415d8ff186ebf7f507f5a7fdbaac094496cd7
Dmitry <dima2002bord@yandex.ru>
2024-05-07 03:46:48 +0300

    make design for registration page

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,13 +1,24 @@
 <template>
-    <div>
-        <p>Email:</p>
-        <input id="email" type="text">
-        <p>Логин:</p>
-        <input id="login" type="text">
-        <p>Пароль:</p>
-        <input id="password" type="text">
-        <button id="refistration-button" @click="registrationAccount">Зарегистрироваться</button>
+    <NavBar></NavBar>
+    <div class="wrapper">
+        <div id="registration-content">
+            <h1>Регистрация</h1>
+            <div class="label-and-fields-block">
+                <div class="data-labels">
+                    <p>Email:</p>
+                    <p>Логин:</p>
+                    <p>Пароль:</p>
+                </div>
+                <div class="data-fields">
+                    <input id="email" type="text">
+                    <input id="login" type="text">
+                    <input id="password" type="text">
+                </div>
+            </div>
+            <button class="button-shadow button-general button-default-border" id="refistration-button" @click="registrationAccount">Зарегистрироваться</button>
+        </div>
     </div>
+
 </template>
 
 <script>
@@ -15,8 +26,12 @@
 import { loginAccount, registration_account } from '../network';
 import { tokenIsSet } from '../validation';
 import router from '../router';
+import NavBar from '@/components/NavBar.vue';
 
 export default {
+    components: {
+        NavBar
+    },
     data() {
         return {
             status: ""
@@ -37,3 +52,67 @@ export default {
 }
 
 </script>
+
+<style>
+#registration-content {
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 80%;
+    /* not working yet */
+    /* height: 100%; */
+    padding: 50px;
+    gap: 5em;
+    box-shadow:
+        0 1px 1px hsl(0deg 0% 0% / 0.075),
+        0 2px 2px hsl(0deg 0% 0% / 0.075),
+        0 4px 4px hsl(0deg 0% 0% / 0.075),
+        0 8px 8px hsl(0deg 0% 0% / 0.075),
+        0 16px 16px hsl(0deg 0% 0% / 0.075);
+    color: white;
+    background-color: var(--bright-background);
+    border-radius: 5px;
+    margin: 0px;
+    /* padding: 0px; */
+}
+
+.label-and-fields-block {
+    display: flex;
+    padding: 0 7em;
+    width: 25em;
+}
+
+.data-labels {
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
+    margin: 0;
+}
+
+.data-labels p {
+    
+}
+
+.data-fields {
+    margin: 1em;
+    display: flex;
+    flex-direction: column;
+    gap: 2.2em;
+    margin: 11px 20px;
+}
+
+.data-fields input {
+    border-radius: 5px;
+    padding: 5px;
+    width: 22em;
+}
+
+#refistration-button {
+    padding: 10px;
+    margin-bottom: 7em;
+    width: 30em;
+}
+
+</style>
